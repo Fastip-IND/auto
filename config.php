e9fef624d89d15fe1c31d225c8bd090954d804b7
Fastip-IND <90595726+Fastip-IND@users.noreply.github.com>
2021-09-13 13:01:31 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+// JANGAN MENGUBAH APAPUN SELAIN YG DIDALAM TANDA (")
+// PERHATIKAN TANDA (") JANGAN SAMPAI TERHAPUS
+// AGAR SCRIPT TIDAK ERROR
+
+$user_agent = "Mozilla/5.0 (Linux; Android 7.1.2; Redmi 4A Build/N2G47H; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/93.0.4577.62 Mobile Safari/537.36";
+
+$cookies = "_immortal|Arc_nodeId=Q8u8HXarM2VmVUQEz1Cgrj; csrf_cookie_name=95a4c8890e7869b276fc92382c2050e3; ci_session=f885bce70591d9bbbed1cf1da901cda4f1c45da3; _data_cpc=132-1_201-1_230-2";
+
